572e42571e0da08be72da6de032eff221f98e364
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-04-23 10:46:31 -0700

    Asp.Net Core Background Task

---
+++ SomeBackgroundTask.cs
@@ -0,0 +1,24 @@
+﻿namespace BackgroundTask.Tasks
+{
+    public class SomeBackgroundTask : BackgroundService
+    {
+        private readonly TimeSpan _period = TimeSpan.FromSeconds(5);
+        private readonly ILogger<SomeBackgroundTask> _logger;
+
+        public SomeBackgroundTask(ILogger<SomeBackgroundTask> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var periodTimer = new PeriodicTimer(_period);
+
+            while (!stoppingToken.IsCancellationRequested &&
+                   await periodTimer.WaitForNextTickAsync(stoppingToken))
+            {
+                _logger.LogInformation("Background Task is executing...");
+            }
+        }
+    }
+}
